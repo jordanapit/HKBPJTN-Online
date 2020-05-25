244842dfa53a4c50faaea9f66f201fd587e57a8e
Jordandreas <jordanapit@gmail.com>
2020-05-25 20:51:33 +0700

    - Add additional text to No Internet Connection, since it appears after downloading PDF, even the connection is available. - Bring ability to watch YouTube video in Fullscreen-Landscape. - Write code to open YouTube link in app (not working yet - needs improvement). - Removed useless code. - Bug fixes and other improvements.

--- app/src/main/java/com/hkbp/jtn/online/MainActivity.java
+++ app/src/main/java/com/hkbp/jtn/online/MainActivity.java
@@ -1,12 +1,23 @@
 package com.hkbp.jtn.online;
 
+//SEBELUMNYA, UNTUK SEMUA YANG BACA CODING INI, SEMUA CODING YANG ADA DI SINI ADALAH HASIL COPPAS DARI WEBSITE.
+//SAYA, JORDAN NAPITUPULU, BUKAN PROGRAMMER, SEHINGGA TIDAK TERLALU PAHAM MENGENAI CODING DI SINI.
+//JIKA TEMAN-TEMAN KONTRIBUTOR INGIN MEMBUANG CODE YANG USELESS, MONGGO SILAHKAN.
+//TAPI JANGAN LUPA KASIH TAU APA YANG KALIAN HAPUS. MAULIATE.
+
+//APA AJA YANG PERLU DIIMPORT DI JAVA INI
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.DownloadManager;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -15,37 +26,62 @@ import android.view.View;
 import android.webkit.CookieManager;
 import android.webkit.DownloadListener;
 import android.webkit.URLUtil;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
-
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
+//CODINGNYA MULAI DARI SINI
+public class MainActivity extends AppCompatActivity {
+    //NGASIH PERIJINAN DI AWAL UNTUK CEK PERMISSION
+    @RequiresApi(api = Build.VERSION_CODES.P)
     private static final int MY_PERMISSION_REQUEST_CODE = 123;
+
+    //MENENTUKAN IDENTITAS
     WebView webviewku;
     WebSettings websettingku;
-
     private Context mContext;
     private Activity mActivity;
-
     private RelativeLayout mRootLayout;
 
 
-
+    //CODING DI DALAM
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //NGASIH TAU YANG MANA YANG ADA DI LAYOUT
         webviewku = (WebView)findViewById(R.id.WebView1);
+        final ProgressBar progressBar = findViewById(R.id.progressBar);
 
+        //NGATUR WEBSETTING DARI WEBVIEWNYA
         websettingku = webviewku.getSettings();
+        websettingku.setJavaScriptEnabled(true);
+        websettingku.setAllowFileAccess(true);
+        websettingku.setAppCacheEnabled(true);
+        websettingku.setLoadsImagesAutomatically(true);
+        websettingku.setDomStorageEnabled(true);
+        websettingku.setLoadWithOverviewMode(true);
+        websettingku.setUseWideViewPort(true);
 
+
+
+        //UNTUK TANDA APAKAH HPNYA ADA KONEKSI INTERNET ATAU ENGGAK
         webviewku.setWebViewClient(new WebViewClient() {
             public void onReceivedError(WebView webView, int errorCode, String description, String failingUrl) {
                 try {
@@ -58,67 +94,113 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 webView.loadUrl("about:blank");
-                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+
+                final AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
                 alertDialog.setTitle("Error");
-                alertDialog.setMessage("Anda tidak memiliki koneksi internet. Sambungkan perangkat anda ke internet dan coba lagi.");
-                alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "Coba Lagi", new DialogInterface.OnClickListener() {
+                alertDialog.setMessage("Anda tidak memiliki koneksi internet, atau halaman gagal dimuat. Pilih 'Refresh' untuk kembali ke Halaman Utama.");
+                alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "Refresh", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         finish();
                         startActivity(getIntent());
                     }
                 });
-
                 alertDialog.show();
                 super.onReceivedError(webView, errorCode, description, failingUrl);
             }
+            //BIKIN YOUTUBE APP KEBUKA
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.contains("youtu.be")){
+
+                    int indexStart = url.indexOf("?v=")+3;
+                    int indexEnd = url.indexOf("&", indexStart);
+                    if(indexEnd<indexStart)
+                        return false;
+                    String videoId = url.substring(indexStart,indexEnd);
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube://"+videoId)));
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
         });
 
+        //UNTUK NGASIH TAU DI WEBVIEWNYA BOLEH/GAK BOLEH NGAPAIN AJA
         webviewku.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
                 return true;
             }
         });
-
         webviewku.setLongClickable(false);
-
         webviewku.setHapticFeedbackEnabled(false);
 
-        webviewku.getSettings().setJavaScriptEnabled(true);
-
-
+        //INI KUNCINYA! NGASIH TAU WEBVIEWNYA HARUS BUKA APA. KALAU KELAK WEBNYA HKBPJTN GANTI DOMAIN, INI JUGA HARUS DIGANTI
         webviewku.loadUrl("https://www.hkbpjtn.online/");
 
-
-
-
+        //KEMAMPUAN UNTUK MENDOWNLOAD FILE; SEMENTARA WAKTU PDF WARTA KEUANGAN/WARTA JEMAAT MUSTI DIDOWNLOAD
         webviewku.setDownloadListener(new DownloadListener() {
             @Override
-            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimeType, long contentLength) {
-                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+            public void onDownloadStart(final String s, final String s1, final String s2, final String s3, long I) {
+                Dexter.withActivity(MainActivity.this)
+                        .withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        .withListener(new PermissionListener() {
+                            @Override
+                            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
 
-                request.setMimeType(mimeType);
-                //------------------------COOKIE!!------------------------
-                String cookies = CookieManager.getInstance().getCookie(url);
-                request.addRequestHeader("cookie", cookies);
-                //------------------------COOKIE!!------------------------
-                request.addRequestHeader("User-Agent", userAgent);
-                request.setDescription("Downloading file...");
-                request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimeType));
-                request.allowScanningByMediaScanner();
-                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimeType));
-                DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
-                dm.enqueue(request);
-                Toast.makeText(getApplicationContext(), "Sedang mendownload...", Toast.LENGTH_LONG).show();
+                                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(s));
+                                request.setMimeType(s3);
+                                String cookies = CookieManager.getInstance().getCookie(s1);
+                                request.addRequestHeader("cookie",cookies);
+                                request.addRequestHeader("User-Agent",s1);
+                                request.setDescription("Sedang mengunduh file");
+                                request.setTitle(URLUtil.guessFileName(s,s2,s3));
+                                request.allowScanningByMediaScanner();
+                                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(s,s2,s3));
+                                    DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                                downloadManager.enqueue(request);
+                                    Toast.makeText(MainActivity.this, "Sedang mengunduh file", Toast.LENGTH_SHORT).show();
+
+                            }
+
+                            @Override
+                            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                                permissionToken.continuePermissionRequest();
+                            }
+                        }).check();
             }
         });
 
 
 
+        //NGASIH IDENTITAS UNTUK CODING YOUTUBE BISA FULLSCREEN + TAMPILIN CIRCLE LOADING. CODING FULLSCREENNYA ADA DI BAWAH
+        webviewku.setWebChromeClient(new myChrome() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                if(newProgress < 100){
+                    progressBar.setVisibility(View.VISIBLE);
+                }
+                if(newProgress==100){
+                    progressBar.setVisibility(View.GONE);
+                }
 
+            }
+        });
     }
 
+
+
+    //INI UNTUK NGASIH PESAN KALAU KITA MAU KELUAR DARI APP
     @Override
     public void onBackPressed() {
         if(webviewku.canGoBack()) {
@@ -141,10 +223,9 @@ public class MainActivity extends AppCompatActivity {
                 });
                 AlertDialog alertDialog = builder.create();
                 alertDialog.show();}
-
-
     }
 
+    //INI UNTUK CHECK PERMISSION, APAKAH READ/WRITE STORAGENYA DIPERBOLEHKAN
     protected void checkPermission(){
         if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
             if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
@@ -176,6 +257,48 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 // Permission already granted
             }
+        }
+    }
+
+    //INI CODING UNTUK YOUTUBE BISA FULLSCREEN
+    private class myChrome extends WebChromeClient {
+        private View mCustomView;
+        private WebChromeClient.CustomViewCallback mCustomViewCallback;
+        //protected FrameLayout mFullscreenContainer;
+        //private int mOriginalOrientation;
+        private int mOriginalSystemUiVisibility;
+
+        myChrome() {
+        }
+
+        public Bitmap getDefaultVideoPoster() {
+            if (mCustomView == null) {
+                return null;
+            }
+            return BitmapFactory.decodeResource(getApplicationContext().getResources(), 2130837573);
+        }
+
+        public void onHideCustomView() {
+            ((FrameLayout) getWindow().getDecorView()).removeView(this.mCustomView);
+            this.mCustomView = null;
+            getWindow().getDecorView().setSystemUiVisibility(this.mOriginalSystemUiVisibility);
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            this.mCustomViewCallback.onCustomViewHidden();
+            this.mCustomViewCallback = null;
+        }
+
+        public void onShowCustomView(View paramView, WebChromeClient.CustomViewCallback paramCustomViewCallback) {
+            if (this.mCustomView != null) {
+                onHideCustomView();
+                return;
+            }
+            this.mCustomView = paramView;
+            this.mOriginalSystemUiVisibility = getWindow().getDecorView().getSystemUiVisibility();
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            //this.mOriginalOrientation = getRequestedOrientation();
+            this.mCustomViewCallback = paramCustomViewCallback;
+            ((FrameLayout) getWindow().getDecorView()).addView(this.mCustomView, new FrameLayout.LayoutParams(-1, -1));
+            getWindow().getDecorView().setSystemUiVisibility(3846);
         }
     }
 
