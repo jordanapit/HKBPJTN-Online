44d017e41fb400196216dea9f2a68641ef7f1fee
Jordandreas <jordanapit@gmail.com>
2020-06-07 12:51:40 +0700

    - Bring ability to send notification

--- app/src/main/java/com/hkbp/jtn/online/HomeActivity.java
+++ app/src/main/java/com/hkbp/jtn/online/HomeActivity.java
@@ -1,10 +1,10 @@
 package com.hkbp.jtn.online;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 4000;
